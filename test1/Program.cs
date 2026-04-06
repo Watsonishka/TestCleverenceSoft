cd359fda23e96faab8a9e2e0f69092b9f5e43224
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-06 20:45:53 +0300

    выделила класс WordCompressor. Переписала метод Compress на случай, если в входных данных появятся цифры, используя список кортежей

--- test1/Program.cs
+++ test1/Program.cs
@@ -6,115 +6,11 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var input = "faaaaaaaaaaaagggrrrrrrrrrrr";
-        var compressionWord = CompressWord(input);
+        var input = "faaaaaaaaaaaa***gggrrrrrrrrrrr";
+        var compressionWord = WordCompressor.Compress(input);
         Console.WriteLine(compressionWord);
-        var decompressionWord = DecompressWord(compressionWord);
+        var decompressionWord = WordCompressor.Decompress(compressionWord);
         Console.WriteLine(decompressionWord);
-    }
-    public static string CompressWord(string input)
-    {
-        if (string.IsNullOrEmpty(input))
-        {
-            return "";
-        }
-        if (input.Length == 1)
-        {
-            return input;
-        }
-
-        var previousSymbol = input[0];        
-        var currentSymbol = ' ';
-        var repeatSymbolCount = 1;
-        var compressionWord = new StringBuilder();
-
-        for (var i = 1; i < input.Length; i++)
-        {
-            currentSymbol = input[i];
-
-            if (currentSymbol == previousSymbol)
-            {
-                repeatSymbolCount++;
-            }
-            else
-            {
-                compressionWord.Append(previousSymbol);
-                if (repeatSymbolCount > 1)
-                {
-                    compressionWord.Append(repeatSymbolCount);
-                }
-                repeatSymbolCount = 1;
-                previousSymbol = currentSymbol;
-            }
-            if (i == input.Length - 1)
-            {
-                compressionWord.Append(currentSymbol);
-                if (repeatSymbolCount > 1)
-                {
-                    compressionWord.Append(repeatSymbolCount);
-
-                }
-            }
-        }
-        return compressionWord.ToString();
-    }
-
-    public static string DecompressWord (string input)
-    {
-        if (string.IsNullOrEmpty(input))
-        {
-            return "";
-        }
-        if (input.Length == 1)
-        {
-            return input;
-        }
-
-        var currentSymbol = input[0];
-        var decompressionWord = new StringBuilder();
-        var countSymbols = "";
-        var isNumberStart = false;
-
-        for (var i = 1; i < input.Length; i++)
-        {
-            if (char.IsDigit(input[i]))
-            {
-                isNumberStart = true;
-                countSymbols += input[i].ToString();
-            }
-            else if (!char.IsDigit(input[i]) && isNumberStart)
-            {
-                var count = int.Parse(countSymbols);
-                for (var j = 1; j <= count; j++)
-                {
-                    decompressionWord.Append(currentSymbol);
-                }
-                countSymbols = "";
-                isNumberStart = false;
-                currentSymbol = input[i];
-            }
-            else
-            {
-                decompressionWord.Append(currentSymbol);
-                currentSymbol = input[i];
-            }
-            if (i == input.Length - 1)
-            {
-                if (char.IsDigit(input[i]))
-                {
-                    var count = int.Parse(countSymbols);
-                    for (var j = 1; j <= count; j++)
-                    {
-                        decompressionWord.Append(currentSymbol);
-                    }
-                }
-                else
-                {
-                    decompressionWord.Append(currentSymbol);
-                }
-            }
-        }
-        return decompressionWord.ToString();
-    }
+    }  
 
 }
---
+++ test1/WordCompressor.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+
+namespace Test1ConsoleApp
+{
+    public class WordCompressor
+    {
+        public List<(char symbol, int count)> Compress(string input)
+        {
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return new List<(char symbol, int count)>();
+            }
+
+            var previousSymbol = input[0];
+            var currentSymbol = ' ';
+            var repeatSymbolCount = 1;
+            var compressionWords = new List<(char symbol, int count)>();
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                currentSymbol = input[i];
+
+                if (currentSymbol == previousSymbol)
+                {
+                    repeatSymbolCount++;
+                }
+                else
+                {
+                    compressionWords.Add((previousSymbol, repeatSymbolCount));
+                    repeatSymbolCount = 1;
+                    previousSymbol = currentSymbol;
+                }
+            }
+
+            compressionWords.Add((previousSymbol, repeatSymbolCount));
+            return compressionWords;
+        }
+
+        public static string Decompress(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+            if (input.Length == 1)
+            {
+                return input;
+            }
+
+            var currentSymbol = input[0];
+            var decompressionWord = new StringBuilder();
+            var countSymbols = "";
+            var isNumberStart = false;
+
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    isNumberStart = true;
+                    countSymbols += input[i].ToString();
+                }
+                else if (!char.IsDigit(input[i]) && isNumberStart)
+                {
+                    var count = int.Parse(countSymbols);
+                    for (var j = 1; j <= count; j++)
+                    {
+                        decompressionWord.Append(currentSymbol);
+                    }
+                    countSymbols = "";
+                    isNumberStart = false;
+                    currentSymbol = input[i];
+                }
+                else
+                {
+                    decompressionWord.Append(currentSymbol);
+                    currentSymbol = input[i];
+                }
+                if (i == input.Length - 1)
+                {
+                    if (char.IsDigit(input[i]))
+                    {
+                        var count = int.Parse(countSymbols);
+                        for (var j = 1; j <= count; j++)
+                        {
+                            decompressionWord.Append(currentSymbol);
+                        }
+                    }
+                    else
+                    {
+                        decompressionWord.Append(currentSymbol);
+                    }
+                }
+            }
+            return decompressionWord.ToString();
+        }
+    }
+}
