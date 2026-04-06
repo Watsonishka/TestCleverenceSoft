d51ba7193abecb1af84eee300d7cdc2dfd4ee5c8
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-06 23:44:40 +0300

    Написала InputValidatior который на всякий случай проверяет ввод на маленькие латинские буквы. Отрефакторила методы Compress и Decompress, также выделила их в отдельный класс WordCompressor для разделения ответственности

---
+++ test1/WordCompressor.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Test1ConsoleApp
+{
+    public static class WordCompressor
+    {
+        public static string Compress(string userInput)
+        {
+            if (string.IsNullOrEmpty(userInput))
+            {
+                return "";
+            }
+            var compressedWord = new StringBuilder();
+            var repeatSymbolCount = 1;
+
+            for (int i = 1; i <= userInput.Length; i++)
+            {
+                if (i < userInput.Length && userInput[i] == userInput[i - 1])
+                {
+                    repeatSymbolCount++;
+                }
+                else
+                {
+                    compressedWord.Append(userInput[i - 1]);
+
+                    if (repeatSymbolCount > 1)
+                    {
+                        compressedWord.Append(repeatSymbolCount);
+                    }
+                    repeatSymbolCount = 1;
+                }
+            }
+            return compressedWord.ToString();
+        }
+        public static string Decompress(string userInput)
+        {
+            if (string.IsNullOrEmpty(userInput))
+            {
+                return "";
+            }
+
+            var decompressedWord = new StringBuilder();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                var symbol = userInput[i];
+                var repeatSymbolCount = 0;
+
+                while (i + 1 < userInput.Length && char.IsDigit(userInput[i + 1]))
+                {
+                    i++;
+                    var lastNumber = Convert.ToInt32(Convert.ToString(userInput[i]));
+                    repeatSymbolCount = repeatSymbolCount * 10 + lastNumber;
+                }
+                if (repeatSymbolCount == 0)
+                {
+                    repeatSymbolCount = 1;
+                }
+                decompressedWord.Append(new string(symbol, repeatSymbolCount));
+            }
+
+            return decompressedWord.ToString();
+        }
+    }
+}
