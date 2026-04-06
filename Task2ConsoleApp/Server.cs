3f472e82636ef52c5427a1275e24bdd8d6b73aa7
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-06 22:08:48 +0300

    набросала класс Server, пока потоконебезопасный

---
+++ Task2ConsoleApp/Server.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2ConsoleApp
+{
+    public static class Server
+    {
+        private static int count;
+        public static int Count
+        {
+            get => count;
+            set
+            {
+                count = value;
+            }
+        }
+        public static int GetCount()
+        {
+            return count;
+        }
+        public static void AddToCount(int value)
+        {
+            count += value;
+        }
+    }
+}
