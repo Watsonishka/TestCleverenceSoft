6605e86b506c7bcb2cc2da5326012f2a1d3d0391
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-08 23:56:36 +0300

    пока в класс лог добавила автосвойства

--- Task3ConsoleApp/Log.cs
+++ Task3ConsoleApp/Log.cs
@@ -9,5 +9,10 @@ namespace Task3ConsoleApp
         private LogLevel level;
         private string message;
         private string callingMethod;
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public LogLevel Level { get; set; }
+        public string Message { get; set; }
+        public string CallingMethod { get; set; }
     }
 }
---
+++ Task3ConsoleApp/Parser.cs
@@ -0,0 +1,6 @@
+﻿namespace Task3ConsoleApp
+{
+    public static class Parser
+    {
+    }
+}
