e8707bf886a44f58a4bd76d20d7f2d278e0f9306
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-08 23:50:04 +0300

    написан черновой метод LogsRepository

---
+++ Task3ConsoleApp/LogsRepository.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace Task3ConsoleApp
+{
+    public class LogsRepository
+    {
+        private static readonly List<Log> logs = new List<Log>();
+        private const string logsFilePath = "LogsList.json";
+        private const string problemsFilePath = "problems.txt";
+        static LogsRepository()
+        {
+            if (!File.Exists(logsFilePath))
+            {
+                var jsonData = JsonConvert.SerializeObject(logs);
+                FileManager.RewriteTextFile(logsFilePath, jsonData);
+            }
+            else
+            {
+                var logs = GetAll();
+            }
+        }
+        public static List<Log> GetAll()
+        {
+            return FileManager.ReadTextFile<Log>(logsFilePath);
+        }
+        public static void RewriteFile(List<Log> logs)
+        {
+            var jsonData = JsonConvert.SerializeObject(logs);
+            FileManager.RewriteTextFile(logsFilePath, jsonData);
+        }
+
+    }
+}
