9f303f556ff5a3ceec7988ee5ac2acb0e9409af9
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-08 23:43:44 +0300

    написала логику для перечислителя уровня логирования

--- Task3ConsoleApp/Level.cs
+++ Task3ConsoleApp/Level.cs
@@ -1,14 +1,30 @@
-﻿namespace Task3ConsoleApp
+﻿using Microsoft.VisualBasic;
+
+namespace Task3ConsoleApp
 {
     public class Level
     {
         public enum LogLevel
         {
-            Unknown = 0,
-            Infomation = 1,
-            Warning = 2,
-            Error = 3,
-            Debug = 4
+            unknown = 0,
+            information = 1,
+            warning = 2,
+            error = 3,
+            debug = 4
+        }
+
+        public static LogLevel Parse(string logLevel)
+        {
+            switch (logLevel.ToUpper())
+            {
+                case "INFO": return LogLevel.information;
+                case "INFORMATION": return LogLevel.information;
+                case "WARN": return LogLevel.warning;
+                case "WARNING": return LogLevel.warning;
+                case "ERROR": return LogLevel.error;
+                case "DEBUG": return LogLevel.debug;
+                default: return LogLevel.unknown;
+            }
         }
     }
 }
--- Task3ConsoleApp/Log.cs
+++ Task3ConsoleApp/Log.cs
@@ -1,10 +1,12 @@
-﻿namespace Task3ConsoleApp
+﻿using static Task3ConsoleApp.Level;
+
+namespace Task3ConsoleApp
 {
     public class Log
     {
         private DateOnly date;
         private TimeOnly time;
-        private Level level;
+        private LogLevel level;
         private string message;
         private string callingMethod;
     }
