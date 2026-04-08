2e5024b95cc84df0a5378438f1c8faedd6422b41
Regina Rezyapova <rezyapova.93@bk.ru>
2026-04-08 23:29:48 +0300

    подключила JSON Newton и написала логику в FileManager

--- Task3ConsoleApp/FileManager.cs
+++ Task3ConsoleApp/FileManager.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.Text;
+namespace Task3ConsoleApp;
 
-namespace Task3ConsoleApp
+public class FileManager
 {
-    internal class FileManager
+    public static List<T> ReadTextFile<T>(string filePath)
     {
+        var streamReader = new StreamReader(filePath, Encoding.UTF8);
+        var jsonString = streamReader.ReadToEnd();
+        var collection = JsonConvert.DeserializeObject<List<T>>(jsonString) ?? new List<T>();
+        streamReader.Close();
+        return collection;
+    }
+    public static void RewriteTextFile(string filePath, string text)
+    {
+        var streamWriter = new StreamWriter(filePath, false, System.Text.Encoding.UTF8);
+        streamWriter.WriteLine(text);
+        streamWriter.Close();
     }
 }
